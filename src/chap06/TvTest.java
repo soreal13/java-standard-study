29588a04635e2e73d9a5ee0b30099d053561d5ab
sojin13 <sodyniee@naver.com>
2021-06-03 21:32:42 +0900

    Initial commit

---
+++ src/chap06/TvTest.java
@@ -0,0 +1,25 @@
+package chap06;
+
+class Tv {
+
+    //TV의 속성(멤버변수)
+    String color;
+    boolean power;
+    int channel;
+
+    // 기능(메서드
+    void power() { power = !power; }
+    void channelUp() { ++channel; }
+    void channelDown() { --channel; }
+
+}
+
+class TvTest {
+    public static void main(String[] args) {
+        Tv t;
+        t = new Tv();
+        t.channel = 7;
+        t.channelDown();
+        System.out.println("현재 채널은 " + t.channel + "입니다.");
+    }
+}
